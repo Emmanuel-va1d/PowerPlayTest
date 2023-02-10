2a69df2e3dac976112a2012abcac12a2b481a4ba
Emmanuel Valdovinos Cota <vald.emmanuel04@gmail.com>
2023-02-10 08:33:14 -0800

    Made TeleOp

---
+++ TeamCode/src/main/java/TestTele.java
@@ -0,0 +1,13 @@
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class TestTele extends OpMode {
+    @Override
+    public void init() {
+        
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
